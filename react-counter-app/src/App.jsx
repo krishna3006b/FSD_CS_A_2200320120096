df6ffc0ac74d4119416ec17dcc6c9a9572d5d0df
Krishna <varshney.kv1@gmail.com>
2024-12-04 14:38:54 +0530

    updated counter script

--- react-counter-app/src/App.jsx
+++ react-counter-app/src/App.jsx
@@ -1,11 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes,Route } from "react-router-dom"
+
 const App=()=>{
-  return (
+  return(
     <div>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        
+        <Route path="/" element={<h1>Home Page</h1>}></Route>
+        <Route path="/counter" element={<h1>Counter App</h1>}></Route>
+        <Route path="/stopwatch" element={<h1>Stop Watch App</h1>}></Route>
+        <Route path="*" element={<h1>No Page Available</h1>}></Route>
       </Routes>
     </div>
   )
 }
+export default App;
